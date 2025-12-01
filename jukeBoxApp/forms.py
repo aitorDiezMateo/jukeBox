703d45844980f718d13b889ced1b0eb1225b5a8b
MateoPerezSuarez <mateo.perez.suarez@opendeusto.es>
2025-12-01 13:41:12 +0100

    forms favoritos Final solución para todas las ventanas

--- jukeBoxApp/forms.py
+++ jukeBoxApp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Valoracion, Sugerencia
+from .models import Valoracion, Sugerencia, Banda
 
 
 class ValoracionForm(forms.ModelForm):
@@ -13,12 +13,56 @@ class ValoracionForm(forms.ModelForm):
 
 
 class SugerenciaForm(forms.ModelForm):
+    # Campo personalizado para seleccionar artista existente o "Otro"
+    ARTISTA_CHOICES = [('', '-- Seleccionar artista --')] + \
+                     [(banda.nombre, banda.nombre) for banda in Banda.objects.all().order_by('nombre')] + \
+                     [('otro', 'Otro artista')]
+
+    artista_selector = forms.ChoiceField(
+        choices=ARTISTA_CHOICES,
+        required=False,
+        widget=forms.Select(attrs={
+            'class': 'form-control',
+            'id': 'id_artista_selector'
+        }),
+        label="Artista"
+    )
+
+    # Campo adicional para escribir el nombre del artista cuando se selecciona "Otro"
+    artista_otro = forms.CharField(
+        required=False,
+        max_length=200,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Nombre del artista',
+            'id': 'id_artista_otro'
+        }),
+        label=""
+    )
+
     class Meta:
         model = Sugerencia
-        fields = ['titulo', 'artista', 'enlace', 'comentario']
-        widgets = {
-            'titulo': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Título de la canción'}),
-            'artista': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Artista (opcional)'}),
-            'enlace': forms.URLInput(attrs={'class': 'form-control', 'placeholder': 'Enlace a la canción (YouTube, Spotify...)'}),
-            'comentario': forms.Textarea(attrs={'class': 'form-control', 'rows': 4, 'placeholder': 'Explica por qué sugerir esta canción...'}),
-        }
+        fields = ['titulo', 'enlace', 'comentario']
+        # Nota: artista_selector y artista_otro son campos adicionales no basados en modelo
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        # El campo artista ya fue establecido en clean()
+        instance.artista = self.cleaned_data.get('artista', '')
+        if commit:
+            instance.save()
+        return instance
+
+    def clean(self):
+        cleaned_data = super().clean()
+        artista_selector = cleaned_data.get('artista_selector')
+        artista_otro = cleaned_data.get('artista_otro')
+
+        if artista_selector == 'otro':
+            if not artista_otro:
+                raise forms.ValidationError("Debes especificar el nombre del artista.")
+            cleaned_data['artista'] = artista_otro
+        else:
+            cleaned_data['artista'] = artista_selector
+
+        return cleaned_data
